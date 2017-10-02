b523a9a304b1f472627580ced2e6451bf9069ab3
anon <anon>
2017-10-02 13:09:20 -0500

    made Gui_button hold project specific configurations

--- Gui_button.java
+++ Gui_button.java
@@ -12,5 +12,12 @@ class Gui_button extends JButton{
    public Gui_button(Dimension size){
       this.setVisible(true);
       this.setSize(size);
+      this.setOpaque(false);
+      this.setContentAreaFilled(false);
+      this.setBorderPainted(false);
+      this.setRolloverEnabled(false);
+   }
+   @Override
+   public void paint(Graphics g){
    }
 }
